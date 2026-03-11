17d1d5472f39c567df2464af7596cb2cb6606239
kevinNguyen101 <nguyenkevin1202@gmail.com>
2026-03-11 10:42:13 -0700

    Added Dice loss function

--- EarthRemoteSensingRapidResponse/ERSRR_Model.py
+++ EarthRemoteSensingRapidResponse/ERSRR_Model.py
@@ -23,31 +23,35 @@ import matplotlib.pyplot as plt
 import os
 
 # data preparation
-num_classes = 100
 input_shape = (256, 256, 5)
 output_shape = (256, 256, 1)
 
 # hyperparameters
-learning_rate = 0.01
-weight_decay = 0.001
-batch_size = 2
-num_epochs = 5
+learning_rate = 0.0001
+weight_decay = 0.1
+batch_size = 4
+num_epochs = 20
 image_size = 256 # input is resized to (image_size x image_size) 
-patch_size = 16  # size of patches to extract from input
+patch_size = 8  # size of patches to extract from input
 num_patches = (image_size // patch_size) ** 2
 projection_dim = 64 # set to 64 for small datasets, otherwise 768 or 1024
 num_heads = 4
-upscale_factor = 4
+upscale_factor = 2
 transformer_units = [
     projection_dim * 2,
     projection_dim,
 ] # size of transformer layers
-transformer_layers = 4
-conv_layers = 2
+transformer_layers = 8
+conv_layers = 4
 mlp_head_units = [
     2048,
     1024,
 ] # size of dense layers for final classifier (temp: need to adjust)
+
+# File Directory paths
+dataset_dir = "EarthRemoteSensingRapidResponse/Data Collection/train_test_2_13_good_data"
+checkpoint_dir = "EarthRemoteSensingRapidResponse/tmp/checkpoint_new_data.weights.h5"
+test_image = "EarthRemoteSensingRapidResponse/Dataset/validation/20241010T102941_20241010T103402_T31SES_EMIT_L2B_CH4PLM_001_20241006T090738_003685grid_1.tif"
 
 ####################################################
 # class Patches                                    #
@@ -123,6 +127,14 @@ def mlp(x, hidden_units, dropout_rate):
         x = layers.Dense(units, activation=keras.activations.gelu)(x)
         x = layers.Dropout(dropout_rate)(x)
     return x
+#############################################
+# upsampling_block                          #
+#   - convolutional upsampling block        #
+#############################################
+def upsampling_block(x,filters):
+    x = layers.UpSampling2D(size=(2,2), interpolation="bilinear")(x)
+    x = layers.Conv2D(filters, 3, padding="same",activation="relu")(x)
+    return x
 
 ################################################
 # create_vit_encoder_decoder                   #
@@ -137,45 +149,47 @@ def create_vit_encoder_decoder():
 
     # create multiple layers of the Transformer block
     for _ in range(transformer_layers):
-        x1 = encoded_patches
-        # multi-head attention layer
-        attention_output = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
-        )(x1, x1)
-        # skip connection 1
-        x2 = layers.Add()([attention_output, encoded_patches])
-        # mlp
-        x3 = mlp(x2, hidden_units=transformer_units, dropout_rate=0.1)
-        # skip connection 2
-        encoded_patches = layers.Add()([x3, x2])
+
+        attn = layers.MultiHeadAttention(num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+                                         )(encoded_patches, encoded_patches)
+        encoded_patches = layers.LayerNormalization()(encoded_patches+attn)
+        mlp_output = mlp(encoded_patches, hidden_units=transformer_units, dropout_rate=0.1)
+        encoded_patches = layers.LayerNormalization()(encoded_patches + mlp_output)
 
     # convolutional upscaling
     x = layers.Dense(projection_dim)(encoded_patches)
-    x = layers.Reshape((image_size // patch_size, image_size // patch_size, projection_dim))(encoded_patches)
+    x = layers.Reshape((image_size // patch_size, image_size // patch_size, projection_dim))(x)
 
-    # create multiple layers of conv
-    for i in range(conv_layers):
-        x = layers.Conv2DTranspose(
-            filters = projection_dim // 2*(i+1),
-            kernel_size = 3,
-            strides = upscale_factor,
-            padding = "same",
-            activation = "relu"
-        )(x)
+    num_upsample_blocks = round(math.log(patch_size, 2))
     
-    # final output activation
-    outputs = layers.Conv2DTranspose(
-        filters = 1,
-        kernel_size = 3,
-        strides = 1,
-        padding = "same",
-        activation = "sigmoid"
-    )(x)
+    for i in range(num_upsample_blocks):
+        x = upsampling_block(x, (projection_dim // (2**i)))
+
+    # x = upsampling_block(x, 64)
+    # x = upsampling_block(x, 32) 
+    # x = upsampling_block(x, 16)
+    # x = upsampling_block(x, 8)
+    outputs = layers.Conv2D(1, 1, activation="sigmoid")(x)
     
     model = keras.Model(inputs=inputs, outputs=outputs)
     # print(model.summary)
     
     return model
+
+def dice_coefficient(y_true, y_pred, smooth=1e-6):
+    intersection = keras.ops.sum(y_true * y_pred, axis=[1, 2, 3])
+    union = keras.ops.sum(y_true, axis=[1, 2, 3]) + keras.ops.sum(y_pred, axis=[1, 2, 3])
+    dice = (2. * intersection + smooth) / (union + smooth)
+    return ops.mean(dice)
+
+bce = keras.losses.BinaryCrossentropy()
+
+def dice_loss(y_true, y_pred):
+    return 1 - dice_coefficient(y_true, y_pred)
+
+def hybrid_loss(y_true, y_pred):
+
+    return bce(y_true, y_pred) + dice_loss(y_true, y_pred)
 
 ###################################################
 # run_experiment                                  #
@@ -189,15 +203,16 @@ def run_experiment(model, x_train, y_train, x_test, y_test):
     # compile model
     model.compile(
         optimizer=optimizer,
-        loss = "mean_squared_error",
-        metrics=["mean_absolute_error", "mean_squared_error"]
+        loss = hybrid_loss,
+        metrics=["mean_absolute_error", dice_coefficient]
     )
 
     # save model checkpoint
-    checkpoint_filepath = "EarthRemoteSensingRapidResponse/tmp/checkpoint.weights.h5"
+    checkpoint_filepath = checkpoint_dir
     checkpoint_callback = keras.callbacks.ModelCheckpoint(
         checkpoint_filepath,
         monitor="val_mean_squared_error",
+        mode="min",
         save_best_only=True,
         save_weights_only=True,
     )
@@ -243,7 +258,7 @@ def plot_history(history, item):
 ##################################################################
 def process_dataset():
     # Get path to dataset
-    dataset = "EarthRemoteSensingRapidResponse/Dataset/train_test"
+    dataset = dataset_dir
     
     # Iterate over dataset and open each image with rasterio,
     # then split each image into X and Y and concat to a list
@@ -257,25 +272,25 @@ def process_dataset():
         # open image
         if os.path.isfile(image_path):
             with rasterio.open(image_path) as image:
-                image_data = image.read()
-                image_profile = image.profile
+                image_data = image.read().transpose((1,2,0))
                 
                 # format input data
-                image_data_t = np.array(image_data).transpose((1,2,0))
-                X_split = np.array(image_data_t[:, :, :5])
-                Y_split = np.array(image_data_t[:, :, 5:])
+                X_split = image_data[:, :, :5]
+                Y_split = image_data[:, :, 5:6]
                 
-                # normalize X_split and Y_split - add 9999 (to Y_split only) then divide by max
-                X_split_r = X_split / np.max(X_split)
-                Y_split_r = (Y_split + 9999) / (np.max(Y_split + 9999))
-                
-                X.append(X_split_r)
-                Y.append(Y_split_r)
+                X.append(X_split)
+                Y.append(Y_split)
     
-    X = np.array(X)
-    Y = np.array(Y) 
-    # print(X.shape)
-    # print(Y.shape)
+    X = np.array(X).astype(np.float32)
+    Y = np.array(Y).astype(np.float32)
+
+    x_Max = X.max()
+    X = X / (x_Max + 1e-6) # normalize input data
+
+    y_Min = Y.min()
+    y_Max = Y.max()
+    Y = (Y - y_Min) / (y_Max - y_Min + 1e-6) # normalize output data
+    
     
     # split into 80/20 train test
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
@@ -294,18 +309,13 @@ def process_dataset():
 ####################################################
 def preprocess_image(image_path):
     with rasterio.open(image_path) as image:
-        image_data = image.read()
-
-        image_profile = image.profile
-
-        # format input data
-        image_data_t = np.array(image_data).transpose((1,2,0))
-        image_data_t = image_data_t[:, :, :5]
+        image_data = image.read().transpose(1,2,0)[0:, :, :5]
+        image_data = image_data / (np.max(image_data) + 1e-6)  # normalize input
         
         # The model expects (# samples, 256, 256, 1), expand for batch dimension
-        processed_image = np.expand_dims(image_data_t, axis=0)
+        # processed_image = np.expand_dims(image_data_t, axis=0)
 
-    return processed_image
+    return image_data
 
 #################################################################
 # errsr_model_prediction                                        #
@@ -313,31 +323,76 @@ def preprocess_image(image_path):
 #      and saves it as a kml                                    #
 #################################################################
 def errsr_model_prediction(image_path):
-    checkpoint_filepath = "EarthRemoteSensingRapidResponse/tmp/checkpoint.weights.h5"
+    checkpoint_filepath = checkpoint_dir
 
     model = create_vit_encoder_decoder()
     model.load_weights(checkpoint_filepath)
-    processed_image = preprocess_image(image_path)
-    
-    methane_prediction = model.predict(processed_image)
     
     # print(methane_prediction[0])
     # print(np.array([methane_prediction[0]]).shape)
-    
-    # NOTE: the prediction seems to not be correct, likely due
-    #       to errors with the model implementation (likely conv2dTranspose?).
-    #       Could also be partly due to the small dataset size
 
-    plt.imshow(methane_prediction[0])
-    plt.title("Predicted Methane Plume Map")
-    plt.show()
+    pred = model.predict(preprocess_image(image_path)[None])[0,:,:,0]
+
+    print("Pred min:", pred.min())
+    print("Pred max:", pred.max())
+    print("Pred mean:", pred.mean())
+    print("Pred std:", pred.std())
     
     # get profile of base input image
     with rasterio.open(image_path) as base_img:
         base_profile = base_img.profile
+
+        # rasterio is 1-indexed
+        b = base_img.read(1)
+        g = base_img.read(2)
+        r = base_img.read(3)
+        b11 = base_img.read(4)
+        b12 = base_img.read(5)
+        emit = base_img.read(6)
+
+    # normalize rgb values to 0-255
+    rgb = np.dstack((
+        (((r - r.min()) / (r.max() - r.min())) * 255),
+        (((g - g.min()) / (g.max() - g.min())) * 255),
+        (((b - b.min()) / (b.max() - b.min())) * 255))
+    ).astype('uint8')
+
+    # create and save subplot figure
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(14,3), layout="constrained")
+        
+    axes[0].imshow(
+        np.log1p(pred),
+        cmap="cividis"
+    )
+    axes[0].set_title("Predicted Methane Plume (log scale)")
+    sm = plt.cm.ScalarMappable(cmap="cividis")
+    fig.colorbar(sm, ax=axes[0], shrink=1)
+
+    emit[emit < -1000] = np.nan # set no data values to nan for better visualization
+    emit = np.maximum(emit, 0) # set negative values to 0
+
+    emit_log = np.log1p(np.maximum(emit, 0))
+
+    emit_min = np.nanpercentile(emit_log, 5)
+    emit_max = np.nanpercentile(emit_log, 95)
+    emit_scaled = (emit_log - emit_min) / (emit_max - emit_min) # normalize for better visualization
+    emit_scaled = np.clip(emit_scaled, 0, 1) # clip to [0, 1] range for visualization
+
+    axes[0].set_title("Predicted Methane Plume Map")
+    axes[1].imshow(emit_scaled, cmap="cividis")
+    axes[1].set_title('EMIT Ground Truth (log scale)')
+    axes[2].imshow(rgb)
+    axes[2].set_title('RGB S2 Input (Bands 2, 3, 4)')
+    # axes[3].imshow(b11)
+    # axes[3].set_title('SWIR-1 S2 Input (Band 11)')
+    # axes[4].imshow(b12)
+    # axes[4].set_title('SWIR-2 S2 Input (Band 12)')
+    
+    fig.savefig("EarthRemoteSensingRapidResponse/Predictions/testprediction.png")
+    plt.show()
     
     # file path to save .tif to temporarily
-    temp_path = "EarthRemoteSensingRapidResponse/Predictions/testprediction.tf"
+    temp_path = "EarthRemoteSensingRapidResponse/Predictions/testprediction.tif"
         
     # save new .tif of prediction
     with rasterio.open(
@@ -345,7 +400,7 @@ def errsr_model_prediction(image_path):
         **{**base_profile, "count": 1},
         mode="w"
     ) as file:
-        file.write(np.array(methane_prediction[0]).transpose(2,0,1))
+        file.write(np.expand_dims(pred, axis=0))
         
     # get bounds of temp file
     with rasterio.open(temp_path) as t:
@@ -369,11 +424,6 @@ def errsr_model_prediction(image_path):
     ground_overlay.latlonbox.east = east
     ground_overlay.latlonbox.west = west
     
-    # NOTE: this saves the data as a .kml, but its not visualized properly. 
-    #       ideally we'd overlay the methane with the s2 image and save that,
-    #       but we should probably just focus on visualizing the actual plume
-    #       first for the sake of time
-    
     kml.save("EarthRemoteSensingRapidResponse/Predictions/testprediction.kml")
         
 
@@ -389,12 +439,11 @@ def main():
     history = run_experiment(vit_classifier, x_train, y_train, x_test, y_test)
 
     # Input image into model and give prediction
-    test_image_path = "EarthRemoteSensingRapidResponse/Dataset/validation/20230120T164611_20230120T164959_T15SYT.tif"
+    test_image_path = test_image
     errsr_model_prediction(test_image_path)
 
     # plot metrics
     plot_history(history, "loss")
-    plot_history(history, "mean_squared_error")
     plot_history(history, "mean_absolute_error")
     
     return
